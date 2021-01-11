f72f5aac0701a485a3ad14dc7c99f83d5ea19d1c
root <root@ip-172-31-46-15.ap-south-1.compute.internal>
2021-01-11 11:09:16 +0000

    commit

---
+++ Common.groovy
@@ -0,0 +1,17 @@
+import java.util.regex.Matcher;		
+import java.util.regex.Pattern;		
+import groovy.json.JsonSlurper;
+import groovy.json.JsonSlurperClassic
+import groovy.json.*
+
+import java.util.Map;
+import java.io.Reader;
+import java.util.HashMap;
+
+
+def mobile (){
+ sh """
+ 	echo "9972045653"
+    """
+}
+return this 
