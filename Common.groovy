7524b4628f6d1822d639cf9a9c31c8f843074af1
Vinayjois988 <57514893+Vinayjois988@users.noreply.github.com>
2021-01-11 18:00:18 +0530

    Update Common.groovy

--- Common.groovy
+++ Common.groovy
@@ -20,5 +20,10 @@ def vm_creation (def imageid,def vmcount,def vmtype,def keyname,def securitygrou
     sudo aws ec2 run-instances --image-id ${imageid} --block-device-mappings file:///tmp/maping.json --count ${vmcount} --instance-type ${vmtype} --key-name ${keyname} --security-group-ids ${securitygroupid} --subnet-id ${subnetid}
 """
 }
-   
+def create_name (def instanceid,def vmname){
+  sh """
+     sudo aws ec2 create-tags --resources $instanceid --tags Key=Name,Value= ${vmname}
+ """
+ }
+ 
 return this 
