17a6340b87a072f66b6104202c90132c75780d92
Vinayjois988 <57514893+Vinayjois988@users.noreply.github.com>
2021-01-11 12:29:23 +0530

    update
    add

--- redis.groovy
+++ redis.groovy
@@ -1,1 +1,22 @@
-hi world!
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+import groovy.json.JsonBuilder 
+
+node {
+  def vmname="Redis"
+  def vmsize="20"
+  def vmtype="t2.medium"
+  def ostype="RHEL"
+  def imageid="ami-0a9d27a9f4f5c0efc"
+  def vmcount="6"
+  def keyname="jenkins"
+  def securitygroupid="sg-8ed10bec"
+  def subnetid="subnet-dc5068b4"
+  def rootdir="/home/ec2user"
+  
+  stage ('Loading common Files'){
+    checkout scm
+    common=load "${rootdir}/newpro/common.groovy"
+  }
+}
+  
