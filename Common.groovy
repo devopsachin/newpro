ed4ca258145fbb55e8bcf77c3e9f2f6f691e8c09
Vinayjois988 <57514893+Vinayjois988@users.noreply.github.com>
2021-01-12 09:19:32 +0530

    Update Common.groovy

--- Common.groovy
+++ Common.groovy
@@ -22,7 +22,7 @@ def vm_creation (def imageid,def vmcount,def vmtype,def keyname,def securitygrou
 }
 def create_name (def vmname){
   sh '''
-     aws ec2 describe-instances --output json | grep "InstanceId" | awk '{print $2}' | tr '"' ' ' | tr ',' ' ' > name.txt
+     sudo aws ec2 describe-instances --output json | grep "InstanceId" | awk '{print $2}' | tr '"' ' ' | tr ',' ' ' > name.txt
      instanceid=`cat name.txt`
      for a in $instanceid; do
      sudo aws ec2 create-tags --resources $instanceid --tags Key=Name,Value=${vmname}
